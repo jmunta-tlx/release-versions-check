af83129338b7aecbffaa0dec78a6d03f6cbb4107
Jagadesh Munta <jmunta@trustlogix.io>
2021-10-03 15:08:28 -0700

    fix: fixing the path for release script

--- prepare-release.sh
+++ prepare-release.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/bash -x
+pwd
 echo "new_release_version=$1" > new_release_version.txt
 JAR_VERSION=`echo $1 | cut -d'v' -f2`
 echo "jar = $JAR_VERSION"
-cd /workspaces/${PROJECT}
 mvn versions:set -DnewVersion=$JAR_VERSION
 mvn -B clean package ${MAVEN_OPTS}
